f35bb63b5036d2638116c6a219ec5f80081413d1
Rana Salem <ranatasalem@gmail.com>
2024-07-18 13:15:46 +0100

    convert service bus message to json & write to file

--- app/events/message-processor.js
+++ app/events/message-processor.js
@@ -8,12 +8,21 @@ const messageProcessor = async (data) => {
     await fs.mkdir(dirPath, { recursive: true })
 
     let messages = []
-    const fileContent = await fs.readFile(filePath, 'utf8')
-    messages = JSON.parse(fileContent)
+
+    try {
+      const fileContent = await fs.readFile(filePath, 'utf8')
+      messages = JSON.parse(fileContent)
+    } catch (error) {
+      if (error.code !== 'ENOENT') {
+        throw error
+      }
+    }
+
     messages.push(data)
 
     await fs.writeFile(filePath, JSON.stringify(messages, null, 2))
   } catch (error) {
+    console.error('Error processing message:', error)
     throw new Error(error)
   }
 }
