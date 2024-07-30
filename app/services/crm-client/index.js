c1ff11b2e1e6aff958791fdd3ddb81e5fca42663
Rana Salem <ranatasalem@gmail.com>
2024-07-30 10:46:39 +0100

    update CRM client

--- app/services/crm-client/index.js
+++ app/services/crm-client/index.js
@@ -4,26 +4,10 @@ const createCase = require('./create-case')
 const createOnlineSubmissionActivity = require('./create-online-submission-activity')
 const handleError = require('./handle-error')
 
-class CRMClient {
-  async checkContact (id) {
-    return checkContact(id)
-  }
-
-  async checkOrganisation (id) {
-    return checkOrganisation(id)
-  }
-
-  async createCase (organisationId, contactId, submissionId, type) {
-    return createCase(organisationId, contactId, submissionId, type)
-  }
-
-  async createOnlineSubmissionActivity (body) {
-    return createOnlineSubmissionActivity(body)
-  }
-
-  async handleError (error) {
-    return handleError(error)
-  }
+module.exports = {
+  checkContact,
+  checkOrganisation,
+  createCase,
+  createOnlineSubmissionActivity,
+  handleError
 }
-
-module.exports = CRMClient
