8c35f9b13bd637e2af24b32b3286e66a18d13144
Rana Salem <ranatasalem@gmail.com>
2024-07-30 12:25:05 +0100

    set up message processor

---
+++ app/services/message-processor/index.js
@@ -0,0 +1,29 @@
+const util = require('util')
+const { messageConfig } = require('../config')
+const { MessageReceiver } = require('ffc-messaging')
+const { checkAdditionalCrns } = require('./check-additional-crns')
+const { processMessageToCrm } = require('./process-message-to-crm')
+
+const handleMessage = async (message, receiver) => {
+  try {
+    console.log('Received event:', message.body)
+    await checkAdditionalCrns(message.body)
+    await processMessageToCrm(message.body)
+    await receiver.completeMessage(message)
+  } catch (err) {
+    console.error('Message error', util.inspect(err.message, false, null, true))
+  }
+}
+
+const startMessaging = async () => {
+  let crmReceiver //eslint-disable-line
+  const eventsAction = (message) => handleMessage(message, crmReceiver)
+  crmReceiver = new MessageReceiver(
+    messageConfig.eventsSubscription,
+    eventsAction
+  )
+  await crmReceiver.subscribe()
+  console.info('Running CRM service')
+}
+
+module.exports = { startMessaging }
